329816cb565a117404cd609aeda880607a547eb6
Leandro Diaz <lainz@users.noreply.github.com>
2019-04-25 22:25:21 -0300

    Add files via upload

---
+++ unit1.pas
@@ -0,0 +1,112 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, SQLite3Conn, SQLDB, DB, Forms, Controls, Graphics, Dialogs,
+  DBGrids, DBCtrls, StdCtrls;
+
+type
+
+  { TEmulador }
+
+  TEmulador = class(TSQLQuery)
+  private
+    function GetNombre: string;
+    procedure SetNombre(AValue: string);
+  public
+    property nombre: string read GetNombre write SetNombre;
+  end;
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    DataSource1: TDataSource;
+    DBGrid1: TDBGrid;
+    Edit1: TEdit;
+    SQLite3Connection1: TSQLite3Connection;
+    SQLQuery1: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure DBGrid1EditingDone(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TEmulador }
+
+function TEmulador.GetNombre: string;
+begin
+  Result := FieldByName('nombre').AsString;
+end;
+
+procedure TEmulador.SetNombre(AValue: string);
+begin
+  FieldByName('nombre').AsString := AValue;
+end;
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Edit1.Text := TEmulador(SQLQuery1).nombre;
+  Edit1.Visible := True;
+  Button2.Visible := True;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  SQLQuery1.Edit;
+  TEmulador(SQLQuery1).nombre := Edit1.Text;
+  SQLQuery1.Post;
+  Edit1.Visible := False;
+  Button2.Visible := False;
+end;
+
+procedure TForm1.DBGrid1EditingDone(Sender: TObject);
+begin
+  SQLQuery1.Edit;
+  SQLQuery1.Post;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  SQLite3Connection1.DatabaseName := Application.Location + 'lainz.sqlite';
+  SQLite3Connection1.Connected := True;
+
+  try
+    SQLite3Connection1.ExecuteDirect(
+      'CREATE TABLE IF NOT EXISTS emuladores(id int, nombre varchar(50));');
+    SQLTransaction1.Commit;
+  except
+    on e: Exception do
+      ShowMessage(e.Message);
+  end;
+
+  SQLQuery1.Active := True;
+end;
+
+{procedure TForm1.SQLQuery1AfterPost(DataSet: TDataSet);
+begin
+  SQLQuery1.ApplyUpdates; <- sqoAutoApplyUpdates
+  SQLTransaction1.Commit; <- Options sqoAutoCommit
+  UpdateMode <- upWhereAll (previene error no hay datos que enviar)
+  SQLQuery1.Open; <- sqoKeepOpenOnCommit
+end;}
+
+end.
